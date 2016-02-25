08d55c4fba71cbf36b96915e3a1168d3c3a66be2
Farrukh Khamidov <farruxxamidov@gmail.com>
2016-02-25 13:25:20 +0000

    Созданы два класса Rectangle и Square. добавлены методы

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Farrukh on 23-Feb-16.
+ */
+public class Square {
+
+  public  double l;
+
+  public Square(double l) {
+     this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
+}
