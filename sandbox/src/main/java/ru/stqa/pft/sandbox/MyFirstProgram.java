d3e9a5910f27134aa84f30d9c0812785843d6133
Farrukh Khamidov <farruxxamidov@gmail.com>
2016-02-26 10:08:14 +0000

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -17,15 +17,15 @@ public class MyFirstProgram {
     System.out.println("Расстояние между точкой p1(" + p1.x + ", " + p1.y + ")" + " и " + "p2(" + p2.x + ", " + p2.y + ")"
     + " = " + distance(p1,p2) +'\n');
 
-    
+
     System.out.println("Расстояние между точкой p1(" + p1.x + ", " + p1.y + ")" + " и " + "p2(" + p2.x + ", " + p2.y + ")"
             + " = " + p1.distance(p2));
 
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-  }
+//  public static void hello(String somebody) {
+//    System.out.println("Hello, " + somebody + "!");
+//  }
 
   public static double distance(Point p1, Point p2) {
     double distance = Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
