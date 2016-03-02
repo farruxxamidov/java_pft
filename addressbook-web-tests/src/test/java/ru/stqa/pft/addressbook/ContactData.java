709e8144a91ae6bfe1308cf5ec7345e875a96434
Farrukh Khamidov <farruxxamidov@gmail.com>
2016-03-02 12:16:18 +0000

    Задание №4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String lastName;
+  private final String address;
+  private final String homePhone;
+  private final String mobilePhone;
+  private final String workPhone;
+  private final String email;
+
+  public ContactData(String firstName, String lastName, String address, String homePhone, String mobilePhone, String workPhone, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.homePhone = homePhone;
+    this.mobilePhone = mobilePhone;
+    this.workPhone = workPhone;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomePhone() {
+    return homePhone;
+  }
+
+  public String getMobilePhone() {
+    return mobilePhone;
+  }
+
+  public String getWorkPhone() {
+    return workPhone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
