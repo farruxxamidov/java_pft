33a18be9ee61583b45e044570187b6475c78e992
Farrukh Khamidov <farruxxamidov@gmail.com>
2016-04-17 18:37:41 +0100

    Задание №19: Подготовить новый проект для нового учебного приложения

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+/**
+ * Created by Farrukh on 04-Mar-16.
+ */
+public class TestBase {
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+  FirefoxDriver wd;
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+      try {
+          wd.switchTo().alert();
+          return true;
+      } catch (NoAlertPresentException e) {
+          return false;
+      }
+  }
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+}
